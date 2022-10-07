6dfe7691e5413ae270912c0121a009ecd8a681b6
RamiroDiaz22 <diazbravoramiro@gmail.com>
2022-10-06 22:11:01 -0300

    finalized controllers

--- src/controllers/city.controllers.js
+++ src/controllers/city.controllers.js
@@ -1,5 +1,6 @@
-import axios from "axios";
 import request from "request";
+import { constants } from "../../config.js";
+import Cities from "../models/Cities.js";
 
 //Extrae los datos de la base de datos
 //Devuelve un array vacio en caso de que no se encuentre
@@ -15,6 +16,26 @@ export const weatherData = async (req, res, next) => {
     if (!name) {
       res.status(500).json({ message: "City name is required" });
     }
+
+    // En caso de que exista, los valores seran devueltos
+    const dataDb = await getDb(name.toLowerCase());
+    if (dataDb.length !== 0) {
+      next();
+      return res.status(200).json(dataDb);
+    }
+
+    //Accede a la API en caso de que sea la primera vez que se busca la ciudad
+    const url = `${constants.weatherApi.BASE_URL}${name}&appid=${constants.weatherApi.SECRET_KEY}&lang=sp`;
+
+    request({ url, json: true }, async (error, { body }) => {
+      //Almacena los datos de la cidad en la base de datos, para una nueva llamada futura
+      const newCity = await new Cities({
+        name: body.name.toLowerCase(),
+        temp: body.main.temp,
+      }).save();
+      next();
+      res.status(200).json(newCity);
+    });
   } catch (error) {
     res.status(500).json({ message: error });
   }
