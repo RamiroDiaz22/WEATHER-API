8eee0c1f0521c44957fc0e914f1eb10f1b1b0053
RamiroDiaz22 <diazbravoramiro@gmail.com>
2022-10-06 22:12:39 -0300

    updated route

--- src/routes/cities.routes.js
+++ src/routes/cities.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { weatherData } from "../controllers/city.controllers.js";
 
 const router = Router();
 
-router.get("/");
+router.get("/", weatherData);
 
 export default router;
