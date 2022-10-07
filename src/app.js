e4f8f50c39dd38623949fcc3ba3a66b949ee9aab
RamiroDiaz22 <diazbravoramiro@gmail.com>
2022-10-06 22:03:57 -0300

    first route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import morgan from "morgan";
+import cityRouter from "./routes/cities.routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/", cityRouter);
 
 export default app;
